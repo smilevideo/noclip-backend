1d56e5b8a4c8a9d88c7e7ff8d4099c36f1794d6b
smilevideo <54786632+smilevideo@users.noreply.github.com>
2019-10-24 13:26:29 -0400

    Revert "Signin"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
 
-    render json: @users, include: [:games]
+    render json: @users
   end
 
   # GET /users/1
@@ -15,8 +15,13 @@ class UsersController < ApplicationController
 
   # POST /users
   def create
-    @user = User.find_or_create_by(username: user_params[:username])
-    render json: @user
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
   end
 
   # PATCH/PUT /users/1
