6effb004ab129a7bb9cab2d14982c48446af7ca2
Andrea T <tostonar@gmail.com>
2019-10-23 21:16:22 -0400

    change users create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,13 +15,8 @@ class UsersController < ApplicationController
 
   # POST /users
   def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
+    @user = User.find_or_create_by(username: user_params[:username])
+    render json: @user
   end
 
   # PATCH/PUT /users/1
