fe690452e16a81cde770f881998d447f21dae565
Andrea T <tostonar@gmail.com>
2019-10-23 21:00:47 -0400

    game controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,20 @@
 class GamesController < ApplicationController
+  def index
+    @games = Games.all
+    render json: @games
+  end
+
+  def show
+    render json: @game
+  end
+
+  def create
+    Game.create(user_id: params[:game][:user_id], score: params[:game][:score])
+  end
+
+  private
+
+    def game_params
+      params.require(:game).permit(:user_id, :score)
+    end
 end
