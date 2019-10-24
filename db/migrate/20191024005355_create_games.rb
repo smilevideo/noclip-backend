568081535cae072ea2b9de40098d8830bf7e966b
Andrea T <tostonar@gmail.com>
2019-10-23 22:45:40 -0400

    fix games migration error

--- db/migrate/20191024005355_create_games.rb
+++ db/migrate/20191024005355_create_games.rb
@@ -1,5 +1,6 @@
 class CreateGames < ActiveRecord::Migration[6.0]
   def change
+
     create_table :games do |t|
       t.integer :score
       t.references :user, null: false, foreign_key: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_21_134135) do
+ActiveRecord::Schema.define(version: 2019_10_24_005355) do
+
+  create_table "games", force: :cascade do |t|
+    t.integer "score"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_games_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -18,4 +26,5 @@ ActiveRecord::Schema.define(version: 2019_10_21_134135) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "games", "users"
 end
