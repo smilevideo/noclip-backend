a93ed457c1edb9c265aedd6fbf748d146dc25edc
andrewkim <andrewk1216@gmail.com>
2019-11-09 22:51:15 -0500

    switched to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_10_24_181908) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "scores", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "score"
